8f320f472c2f0b01ec9c9626c03aba09a7432ee1
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-01-11 20:49:40 +0800

    Update common.h

--- include/common.h
+++ include/common.h
@@ -11,20 +11,19 @@
 #include<putbmp.h>
 
 struct unit{
-	char name[10];
-	int type;
-	int num;
-	int hp;
-	int oil;
-	int x,y;
-	int max_move;
-	int atk;
+	char name[10]; //战斗单位名称（中文）
+	int type; //战斗单位类型，统一编码，比如坦克是0等等
+	int num; //战斗单位编号，指在数组里的下标
+	int hp; //剩余血量
+	int oil; //剩余资源
+	int x,y; //位置
+	int max_move; //最大移动距离
+	int atk; //攻击力（打一次对面掉多少血）
 };
 
-int get_dis(struct unit a,struct unit b); //计算两个单位之间的距离
-void print_map(int (*map_type)[13+3]);//打印地图（不含清屏）
-void print_UI();//打印UI（头像、边框）（不含清屏）
-void init(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);//初始化
-//从前往后依次是9*13地图上的土地类型（0是陆地，1是海洋），我方单位，敌方单位，我方单位数量，敌方单位数量
+int get_dis(struct unit a,struct unit b);
+void print_map(int (*map_type)[13+3]);
+void print_UI();
+void init(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
 
 #endif
