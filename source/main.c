dffa3e8259056b148f0b950a4ac83f754df5bc22
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-01-11 20:53:28 +0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -4,10 +4,14 @@
 void main(){
 	char name[20];
 	int gdriver,gmode;
+	int map_type[13+3][9+3];
+	struct unit my[100];
+	struct unit en[100];
+	int num_my;
+    	int num_en;
 	gdriver=VGA;
 	gmode=VGAHI;
 	initgraph(&gdriver,&gmode,"source\\BGI");
 	mouseinit();
-	if(welcome(name)) printf("1\n");
-	else printf("0\n");
+	if(welcome(name)) teach();
 }
