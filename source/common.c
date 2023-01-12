c8140229a35102bd9f3476295fa912b1db826cf1
lmpayc <122422904+lmpayc@users.noreply.github.com>
2023-01-12 13:00:47 +0800

    Update common.c
    完成了对地图的初始化，还有敌方单位和我方单位的各种信息的初始化

--- source/common.c
+++ source/common.c
@@ -45,7 +45,131 @@ void print_UI(){
 /********************
 用于初始化
 形参从前往后依次是9*13地图上的土地类型（0是陆地，1是海洋），我方单位，敌方单位，我方单位数量，敌方单位数量
-XXX写的
+我方单位：士兵、坦克、飞机、运输船。类型：1，2，3，4。HP:100，500，250，400。攻击距离：40，80，120，0    资源：5000，200，500，300  移动距离：40，80，120   数量：20，6，15，5。 攻击力：100，500，400。
+敌方单位：主城、炮台、城墙。类型：1、2、3。HP:3000，500，600。攻击距离:40,80,0  资源：5000，5000，5000 移动距离：0，0，0.  攻击力：100，200，0.
+
+lpc写的
 ********************/
-void init(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en){
-}
+void init(int(*map_type)[13 + 3], struct unit my[], struct unit en[], int *num_my, int *num_en) {
+
+		map_type[1][6]=1;      //地图类型
+		map_type[1][7]=1;
+		map_type[1][8]=1;
+		map_type[1][9]=1;
+		map_type[1][10]=1;
+		map_type[1][11]=1;
+		map_type[1][12]=1;
+		map_type[1][13]=1;
+		map_type[2][4]=1;
+		map_type[2][5]=1;
+		map_type[2][6]=1;
+		map_type[2][7]=1;
+		map_type[2][8]=1;
+		map_type[2][9]=0;
+		map_type[2][10]=0;
+		map_type[2][11]=1;
+		map_type[2][12]=1;
+		map_type[2][13]=1;
+		map_type[3][4]=1;
+		map_type[3][5]=1;
+		map_type[3][6]=1;
+		map_type[3][12]=1;
+		map_type[3][13]=1;
+		map_type[4][5]=1;
+		map_type[4][6]=1;
+		map_type[4][12]=1;
+		map_type[4][13]=1;
+		map_type[5][4]=1;
+		map_type[5][5]=1;
+		map_type[5][6]=1;
+		map_type[5][12]=1;
+		map_type[5][13]=1;
+		map_type[6][5]=1;
+		map_type[6][6]=1;
+		map_type[6][12]=1;
+		map_type[6][13]=1;
+		map_type[7][4]=1;
+		map_type[7][5]=1;
+		map_type[7][6]=1;
+		map_type[7][12]=1;
+		map_type[7][13]=1;
+		map_type[8][4]=1;
+		map_type[8][5]=1;
+		map_type[8][6]=1;
+		map_type[8][7]=1;
+		map_type[8][8]=1;
+		map_type[8][11]=1;
+		map_type[8][12]=1;
+		map_type[8][13]=1;
+		map_type[9][6]=1;
+		map_type[9][7]=1;
+		map_type[9][8]=1;
+		map_type[9][9]=1;
+		map_type[9][10]=1;
+		map_type[9][11]=1;
+		map_type[9][12]=1;
+		map_type[9][13]=1;
+		
+		//我方士兵所有信息
+		my[1].type=1;
+		my[1].hp=100;
+		my[1].num=20;
+		my[1].oil=5000;
+		my[1].atk=100;
+		my[1].max_move=40;
+		my[1].max_attack=40;
+
+		//我方坦克所有信息
+		my[2].type=2;
+		my[2].hp=500;
+		my[2].num=6;
+		my[2].oil=200;
+		my[2].atk=500;
+		my[2].max_move=80;
+		my[2].max_attack=80;
+
+		//我方飞机所有信息
+		my[3].type=3;
+		my[3].hp=250;
+		my[3].num=15;
+		my[3].oil=500;
+		my[3].atk=400;
+		my[3].max_move=120;
+		my[3].max_attack=120;
+    
+		//我方运输船所有信息
+		my[4].type=4;
+		my[4].hp=400;
+		my[4].num=5;
+		my[4].oil=300;
+		my[4].atk=0;
+		my[4].max_move=80;
+		my[4].max_attack=0;
+
+		//敌方主城所有信息
+		en[1].type=1;
+		en[1].hp=3000;
+		en[1].num=1;
+		en[1].oil=5000;
+		en[1].atk=100;
+		en[1].max_move=0;
+		en[1].max_attack=40;
+
+		//敌方炮台所有信息
+		en[2].type=2;
+		en[2].hp=500;
+		en[2].num=14;
+		en[2].oil=5000;
+		en[2].atk=200;
+		en[2].max_move=0;
+		en[2].max_attack=80;
+
+		//敌方城墙所有信息
+		en[3].type=3;
+		en[3].hp=600;
+		en[3].num=7;
+		en[3].oil=5000;
+		en[3].atk=0;
+		en[3].max_move=0;
+		en[3].max_attack=0;
+}		
