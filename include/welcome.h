c948754d102eb5d464f357006cdc8b5e02c779ba
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-01-15 19:57:26 +0800

    Update welcome.h

--- include/welcome.h
+++ include/welcome.h
@@ -2,7 +2,9 @@
 #define _WELCOME_H
 
 #include "common.h"
+#include "info.h"
 
 int welcome(char *s);
+void place(int(*map_type)[13+3], struct unit my[],struct unit en[],int num_my,int num_en);
 
 #endif
