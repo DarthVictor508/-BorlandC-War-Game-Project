3a30aee6cf081346b6a267ae2ca2d6b37cc25020
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-02-26 20:56:30 +0800

    Add files via upload

---
+++ include/myturn.h
@@ -0,0 +1,12 @@
+#ifndef _MYTURN_H
+#define _MYTURN_H
+
+#include<common.h>
+
+struct node{
+	int x,y,rest;
+};
+void bfs(struct unit x,int num,int (*map_type)[13+3],int (*f)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
+void myturn(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
+
+#endif
