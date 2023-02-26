88f9584f019884c6ad35375c00281a33927a3f35
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-02-26 15:33:32 +0800

    Update data.c

--- source/data.c
+++ source/data.c
@@ -1,8 +1,8 @@
 #include "data.h"
 
 /********************
-ÅĞ¶ÏÃû×ÖÊÇ·ñÖ®Ç°´æÔÚ
-lzzĞ´µÄ
+åˆ¤æ–­åå­—æ˜¯å¦ä¹‹å‰å­˜åœ¨
+lzzå†™çš„
 ********************/
 int exist(char *s){
 	int n,i;
@@ -18,8 +18,8 @@ int exist(char *s){
 }
 
 /********************
-Ğ´ÈëĞÂµÄÕ½¶·Êı¾İ²¢ÏÔÊ¾½áÎ²
-lzzĞ´µÄ
+å†™å…¥æ–°çš„æˆ˜æ–—æ•°æ®å¹¶æ˜¾ç¤ºç»“å°¾
+lzzå†™çš„
 ********************/
 void rank(char *s,int cnt){
 	int n,grd[20],i,j,jud=0,t,fin[20],r;
@@ -62,35 +62,40 @@ void rank(char *s,int cnt){
 	setfillstyle(SOLID_FILL,WHITE);
 	bar(0,0,640,480);
 	putbmp(0,0,"source\\picture\\ougen3.bmp");
-	puthz(10,360,"Å·¸ù",32,40,BLACK);
-	puthz(475,455,"µã»÷ÈÎÒâ´¦½áÊøÓÎÏ·",16,18,BLACK);
-	puthz(422,5,"ÓÎÏ·½áÊø",48,50,RED);
+	puthz(10,360,"æ¬§æ ¹",32,40,BLACK);
+	puthz(475,455,"ç‚¹å‡»ä»»æ„å¤„ç»“æŸæ¸¸æˆ",16,18,BLACK);
+	puthz(422,5,"æ¸¸æˆç»“æŸ",48,50,RED);
 	if(cnt!=100){
-		puthz(20,405,"ÄúÌ«¾ŞÁË£¡",24,30,BLACK);
-		puthz(418,64,"ÄúÓÃÁË",16,18,BLACK);
-		gotoxy(60,5);
-		printf("%5d",cnt);
-		puthz(523,64,"»ØºÏÈ¡µÃÊ¤Àû",16,18,BLACK);
+		puthz(20,405,"æ‚¨å¤ªå·¨äº†ï¼",24,30,BLACK);
+		puthz(418,64,"æ‚¨ç”¨äº†",16,18,BLACK);
+		setfillstyle(SOLID_FILL, WHITE);
+		setlinestyle(SOLID_LINE, 0, NORM_WIDTH);
+		setcolor(BLACK);
+		settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+		settextjustify(LEFT_TEXT, TOP_TEXT);
+		nts(cnt,temp);
+		outtextxy(485, 65, temp);
+		puthz(520,64,"å›åˆå–å¾—èƒœåˆ©",16,18,BLACK);
 		for(i=1;i<=n;i++){
 			if(strcmp(name[i],s)==0){
 				r=i;
 				break;
 			}
 		}
-		puthz(418,102,"ÄúÄ¿Ç°ÎªÖ¹µÄ×îºÃ³É¼¨ÔÚËùÓĞÍæ¼ÒÖĞÅÅÃûµÚ",16,18,BLACK);
-		gotoxy(69,9);
-		printf("%2d",fin[r]);
-		puthz(450,150,"ÅÅĞĞ°ñ",48,50,RED);
+		puthz(418,102,"æ‚¨ç›®å‰ä¸ºæ­¢çš„æœ€å¥½æˆç»©åœ¨æ‰€æœ‰ç©å®¶ä¸­æ’åç¬¬",16,18,BLACK);
+		nts(fin[r],temp);
+		outtextxy(565, 129, temp);
+		puthz(450,150,"æ’è¡Œæ¦œ",48,50,RED);
 		for(i=1;i<=(n<=10?n:10);i++){
 			gotoxy(54,14+i-1);
 			printf("%2d %-15s %-5d\n",fin[i],name[i],grd[i]);
 		}
 	}
 	else{
-		puthz(20,405,"ÕâÃ´¼òµ¥µÄÕ½¶·¶¼Ó®²»ÁËÂğ¡­¡­",24,30,BLACK);
-		puthz(20,435,"ÕæÊÇÔÓÓãÖ¸»Ó¹Ù¡£",24,30,BLACK);
-		puthz(418,74,"·Ç³£ÒÅº¶",48,50,BLACK);
-		puthz(418,150,"ÄúÕ½°ÜÁË",48,50,BLACK);
+		puthz(20,405,"è¿™ä¹ˆç®€å•çš„æˆ˜æ–—éƒ½èµ¢ä¸äº†å—â€¦â€¦",24,30,BLACK);
+		puthz(20,435,"çœŸæ˜¯æ‚é±¼æŒ‡æŒ¥å®˜ã€‚",24,30,BLACK);
+		puthz(418,74,"éå¸¸é—æ†¾",48,50,BLACK);
+		puthz(418,150,"æ‚¨æˆ˜è´¥äº†",48,50,BLACK);
 	}
 	while(1){
 		newmouse(&MouseX,&MouseY,&press);
