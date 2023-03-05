42e7269f445afae2fb7505510e17b9804ce7456f
lmpayc <122422904+lmpayc@users.noreply.github.com>
2023-03-05 12:57:41 +0800

    Add files via upload

---
+++ include/enturn.h
@@ -0,0 +1,19 @@
+#ifndef _ENTURN_H
+#define _ENTURN_H
+
+#include"common.h"
+#include"anime.h"
+
+struct ab_atk {
+    int x;   //被搜到的攻击单位格子坐标
+    int y;
+    int code;
+    int type;
+};
+
+int enturn(struct unit my[], struct unit en[]);
+void en_search(struct unit my[], struct unit en[], int en_atk[], struct ab_atk able[]);
+void en_print_atk(struct unit my[], struct unit en[],int en_atk[], struct ab_atk able[]);
+void en_p_dis_blood(struct unit my[], struct unit en[], struct ab_atk able[]);
+
+#endif
