dc973dd247a5f1ea83ff20440a6456bf5fe2e257
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-03-07 16:31:32 +0800

    Update myturn.h

--- include/myturn.h
+++ include/myturn.h
@@ -7,6 +7,6 @@ struct node{
 	int x,y,rest;
 };
 void bfs(struct unit x,int num,int (*map_type)[13+3],int (*f)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
-void myturn(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
+int myturn(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
 
 #endif
