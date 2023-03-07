3721c35fa25d58d332fafbe623677b57841d9e6f
lmpayc <122422904+lmpayc@users.noreply.github.com>
2023-03-07 16:01:37 +0800

    Update enturn.h

--- include/enturn.h
+++ include/enturn.h
@@ -5,14 +5,14 @@
 #include"anime.h"
 
 struct ab_atk {
-    int x;   //被搜到的攻击单位格子坐标
+    int x;   //琚悳鍒扮殑鏀诲嚮鍗曚綅鏍煎瓙鍧愭爣
     int y;
     int code;
     int type;
 };
 
 int enturn(struct unit my[], struct unit en[]);
-void en_search(struct unit my[], struct unit en[], int en_atk[], struct ab_atk able[]);
+void en_p_search(struct unit my[], struct unit en[], int en_atk[], struct ab_atk able[]);
 void en_print_atk(struct unit my[], struct unit en[],int en_atk[], struct ab_atk able[]);
 void en_p_dis_blood(struct unit my[], struct unit en[], struct ab_atk able[]);
 
