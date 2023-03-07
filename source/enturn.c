0760caf01531fb8bfc07da3acdcedf2290688023
lmpayc <122422904+lmpayc@users.noreply.github.com>
2023-03-07 20:40:54 +0800

    Update enturn.c

--- source/enturn.c
+++ source/enturn.c
@@ -4,16 +4,26 @@
 /*
 敌方回合函数
 */
-int enturn(struct unit my[], struct unit en[]) {
+int enturn(int (*map_type)[13 + 3],struct unit my[], struct unit en[],int num_my) {
 	int en_atk[15] = {0};            //发出攻击的敌方单位信息
 	struct ab_atk able[15];
+	int i,j,dea_my=0;
+	en_turn();                    //更新侧边栏
+	mine_boom(my, en, num_my);
 	en_p_search(my, en, en_atk, able);
-	en_print_atk(my, en, en_atk, able);
+	en_print_atk(my, en, en_atk, able,num_my, map_type);
 	en_p_dis_blood(my, en, able);
-	return 1;
+
+	for (i = 1; i <= 3; i++) {
+		for (j = 1; j <= my[i].num; j++) {
+			if (my[i].hp[j] <= 0) {
+				dea_my++;
+			}
+		}
+	}
+	if (dea_my == (my[1].num + my[2].num + my[3].num)) return 0;  //表示游戏失败
+	return 1;           //游戏继续
 }
-
-
 
 /*
 敌方炮台搜索攻击单位函数
@@ -43,7 +53,7 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 				}
 				else flag1 = 0;
 			}
-			if (abs(my[3].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[3].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[3].hp[j]>0) {
+			if (abs(my[3].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[3].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[3].hp[j]>0 && en[2].hp[i]>0) {
 				able[k].x = my[3].a[j];
 				able[k].y = my[3].b[j];
 				able[k].code = j;
@@ -85,7 +95,7 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 						}
 						else flag1 = 0;
 					}
-					if (abs(my[2].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[2].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[2].hp[j] > 0) {
+					if (abs(my[2].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[2].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[2].hp[j] > 0 && en[2].hp[i] > 0) {
 
 						able[k].x = my[2].a[j];
 						able[k].y = my[2].b[j];
@@ -130,7 +140,7 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 						}
 						else flag1 = 0;
 					}
-					if (abs(my[1].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[1].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[1].hp[j] > 0) {
+					if (abs(my[1].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[1].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[1].hp[j] > 0 && en[2].hp[i] > 0) {
 
 						able[k].x = my[1].a[j];
 						able[k].y = my[1].b[j];
@@ -175,7 +185,7 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 						}
 						else flag1 = 0;
 					}
-					if (abs(my[4].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[4].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[4].hp[j] > 0) {
+					if (abs(my[4].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[4].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[4].hp[j] > 0 && en[2].hp[i] > 0) {
 
 						able[k].x = my[4].a[j];
 						able[k].y = my[4].b[j];
@@ -220,7 +230,7 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 						}
 						else flag1 = 0;
 					}
-					if (abs(my[5].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[5].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[5].hp[j] > 0) {
+					if (abs(my[5].a[j] - en[2].a[i]) <= en[2].max_attack && abs(my[5].b[j] - en[2].b[i]) <= en[2].max_attack && flag1 == 0 && my[5].hp[j] > 0 && en[2].hp[i] > 0) {
 
 						able[k].x = my[5].a[j];
 						able[k].y = my[5].b[j];
@@ -240,17 +250,27 @@ void en_p_search(struct unit my[], struct unit en[],int en_atk[], struct ab_atk 
 
 }
 
-void en_print_atk(struct unit my[], struct unit en[], int en_atk[], struct ab_atk able[]) {
+
+/*
+打印敌方炮台攻击动画函数
+*/
+void en_print_atk(struct unit my[], struct unit en[], int en_atk[], struct ab_atk able[],int num_my, int(*map_type)[13 + 3]) {
 	int i;
 	for (i = 0; i < en[2].num; i++) {
 		if (able[i].x == 0) break;
-		delay(500);
+		delay(500);	
+		print_map(map_type);
+		print_my(my, num_my);
+		print_en(en);
 		print_atk(en[2].x[en_atk[i]], en[2].y[en_atk[i]], able[i].x * 40-20 , able[i].y * 40-20 );
 		delay(100);
-		cleardevice;
 	}
 }
 
+
+/*
+扣除我方单位被炮台攻击血量函数
+*/
 void en_p_dis_blood(struct unit my[], struct unit en[], struct ab_atk able[]) {
 	int i,j;
 	for (i = 0; i < en[2].num; i++) {
@@ -258,5 +278,46 @@ void en_p_dis_blood(struct unit my[], struct unit en[], struct ab_atk able[]) {
 			break;
 		}
 		my[able[i].type].hp[able[i].code] -= en[2].atk;
+	}
+}
+
+
+/*
+敌方地雷攻击函数（含动画）
+*/
+
+void mine_boom(struct unit my[], struct unit en[],int num_my) {
+	int i, j,k;
+	for (i = 1; i <= num_my; i++) {
+		if ((i == 3) || (i == 4)) continue;
+		for (j = 1; j <= my[i].num; j++) {
+			if (my[i].a[j] == en[5].a[1] && my[i].b[j] == en[5].b[1] && en[5].hp[1] > 0) {   
+				print_boom(en[5].x[1], en[5].y[1]);           //一号雷爆炸
+				delay(500);
+				my[i].hp[j] -= en[5].atk;
+				en[5].hp[1] = 0;
+			}
+
+			if (my[i].a[j] == en[5].a[2] && my[i].b[j] == en[5].b[2] && en[5].hp[2] > 0) {
+				print_boom(en[5].x[2], en[5].y[2]);           //二号雷爆炸
+				delay(500);
+				my[i].hp[j] -= en[5].atk;
+				en[5].hp[2] = 0;
+			}
+
+			if (my[i].a[j] == en[5].a[3] && my[i].b[j] == en[5].b[3] && en[5].hp[3] > 0) {
+				print_boom(en[5].x[3], en[5].y[3]);           //三号雷爆炸
+				delay(500);
+				my[i].hp[j] -= en[5].atk;
+				en[5].hp[3] = 0;
+			}
+
+			if (my[i].a[j] == en[5].a[4] && my[i].b[j] == en[5].b[4] && en[5].hp[4] > 0) {
+				print_boom(en[5].x[4], en[5].y[4]);          //四号雷爆炸
+				delay(500);
+				my[i].hp[j] -= en[5].atk;
+				en[5].hp[4] = 0;
+			}
+		}
 	}
 }
