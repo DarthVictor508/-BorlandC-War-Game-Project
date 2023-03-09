99721a720fd221ef8f480abc675020de31bcb0fb
lmpayc <122422904+lmpayc@users.noreply.github.com>
2023-03-09 13:42:26 +0800

    Update welcome.h

--- include/welcome.h
+++ include/welcome.h
@@ -6,6 +6,6 @@
 
 int welcome(char *s);
 void place(int(*map_type)[13+3], struct unit my[],struct unit en[],int num_my,int num_en);
-void teach();
+void teach(int(*map_type)[13 + 3], struct unit my[], struct unit en[], int num_my, int num_en);
 
 #endif
