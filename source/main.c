3d7e39b7a7370562d68cd336db52652f4ff03575
DarthVictor508 <65144081+DarthVictor508@users.noreply.github.com>
2023-03-11 19:13:35 +0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,17 +1,47 @@
 #include"common.h"
 #include"welcome.h"
+#include"info.h"
+#include"myturn.h"
+#include"anime.h"
 
 void main(){
-	char name[20];
+	char name[20],tmp[3];
 	int gdriver,gmode;
-	int map_type[13+3][9+3];
-	struct unit my[100];
-	struct unit en[100];
-	int num_my;
-    	int num_en;
+	int map_type[13+3][13+3];
+	int f[13+3][13+3];
+	struct unit my[10];
+	struct unit en[10];
+	int num_my=5;
+	int num_en=5;
+	int x,y,op;
+	int cnt=0;
+
 	gdriver=VGA;
 	gmode=VGAHI;
 	initgraph(&gdriver,&gmode,"source\\BGI");
 	mouseinit();
-	if(welcome(name)) teach();
+
+	init(map_type,my,en);
+	if(welcome(name)) teach(map_type,my,en,num_my,num_en);
+	init(map_type,my,en);
+	print_UI();
+	print_map(map_type);
+	print_en(en);
+	place(map_type,my,en,num_my,num_en);
+	while(++cnt){
+		if(cnt==20) break;
+		puthz(122,361,"当前为第",16,18,WHITE);
+		puthz(227,361,"回合",16,18,WHITE);
+		nts(cnt,tmp);
+		setcolor(WHITE);
+		settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+		settextjustify(LEFT_TEXT, TOP_TEXT);
+		outtextxy(196,361,tmp);
+		if(myturn(map_type,my,en,num_my,num_en)) break;
+		if(!enturn(map_type,my,en,num_my)){
+			cnt=20;
+			break;
+		}
+	}
+	rank(name,cnt);
 }
