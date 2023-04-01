20335809e377b7a37c4f1359a21314316527cfc3
DarthVictor508 <1326659192@qq.com>
2023-04-01 21:27:04 +0800

    重构文件结构

--- Project/BC31/DISK_C/Project/include/pvp.h
+++ Project/BC31/DISK_C/Project/include/pvp.h
@@ -2,6 +2,7 @@
 #define _PVP_H
 
 #include "common.h"
+#include"mytur.h"
 
 void pvp(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en);
 
--- Project/BC31/DISK_C/Project/source/pvp.c
+++ Project/BC31/DISK_C/Project/source/pvp.c
@@ -1,4 +1,8 @@
 #include"pvp.h"
 
+/********************
+双人对战模式
+lzz写的
+********************/
 void pvp(int (*map_type)[13+3],struct unit my[],struct unit en[],int num_my,int num_en){
 }
